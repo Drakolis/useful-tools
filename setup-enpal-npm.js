059c601c41d3cf9ce62ad69b6a38e827a4389d53
Mika Zimina <mika.zimina@enpal.de>
2021-12-16 10:28:31 +0100

    Add script to set up auth for npm feed in Azure

---
+++ setup-enpal-npm.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const os = require('os')
+
+const token = process.argv[2]; // 0 - node, 1 - script name
+console.log(token, "Token");
+
+if(!token) {
+  console.error("No token was given!");
+  process.exit();
+}
+
+const encodedToken = Buffer.from(token.trim()).toString('base64');
+console.log(encodedToken, "Encoded Token");
+
+const pathToUserNpmRc = os.homedir() + "/.npmrc";
+
+const content = `
+; begin auth token 
+//pkgs.dev.azure.com/enpal/_packaging/enpal%40Local/npm/registry/:username=enpal
+//pkgs.dev.azure.com/enpal/_packaging/enpal%40Local/npm/registry/:_password=${encodedToken}
+//pkgs.dev.azure.com/enpal/_packaging/enpal%40Local/npm/registry/:email=npm requires email to be set but doesn't use the value
+//pkgs.dev.azure.com/enpal/_packaging/enpal%40Local/npm/:username=enpal
+//pkgs.dev.azure.com/enpal/_packaging/enpal%40Local/npm/:_password=${encodedToken}
+//pkgs.dev.azure.com/enpal/_packaging/enpal%40Local/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
+`
+
+fs.writeFileSync(pathToUserNpmRc, content /*, { flag: 'a+' } */);
+
+console.log("Done!");
+
+const result = fs.readFileSync(pathToUserNpmRc).toString();
+
+console.log(result);
