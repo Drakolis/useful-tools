2df002d34476e0357cada1076f85b60d0dd47586
Mika Zimina <m.zimina@singenetwork.com>
2022-10-23 18:49:48 +0200

    Add collect licenses script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # useful-tools
 My scripts
+
+## setup-enpal-npm
+Old work stuff, do not use
+
+## collect-licenses
+Collects repository licenses and writes them to LICENSES.md file
---
+++ collect-licenses.js
@@ -0,0 +1,63 @@
+const { readdirSync, readFileSync, writeFileSync, appendFileSync } = require("fs");
+const { join } = require("path");
+
+const projectPath = process.argv[2]; // 0 - node, 1 - script name
+
+const path = join(projectPath, "node_modules")
+const packageFile = "package.json"
+const modules = readdirSync(path);
+const licensesMdPath = join(projectPath, "LICENSES.MD")
+
+const properModules = modules.filter(module => !module.startsWith(".")).reduce((accumulator, module) => {
+  if(!module.includes("@")) {
+    return [...accumulator, module]
+  } else {
+    const submodules = readdirSync(join(path, module)).map(submodule => join(module, submodule));
+    return [...accumulator, ...submodules]
+  }
+}, []);
+
+const moduleData = properModules.map((module) => {
+  const packageJSON = JSON.parse(readFileSync(join(path, module, packageFile)));
+  const name = packageJSON.name || "noname:" + module;
+  const license = packageJSON.license || "unknown license";
+  const version = packageJSON.version || "unknown version";
+  const author = packageJSON.author || "unknown author";
+  return {
+    name, license, version, author
+  }
+});
+
+writeFileSync(licensesMdPath, `
+Name | Version | License | Author
+--- | --- | --- | ---
+`)
+
+const licenseData = {}
+
+moduleData.forEach(data => {
+  let authorString = data.author;
+  if(typeof authorString === "object") {
+    if (!Object.keys(authorString).includes("name") && !Object.keys(authorString).includes("email") && !Object.keys(authorString).includes("url")) {
+      console.log(JSON.stringify(authorString));
+      if (Array.isArray(authorString)) {
+        authorString = authorString.join(", ")
+      }
+    }
+    authorString = `${authorString.name} <${authorString.email}> [${authorString.url}]`
+  }
+  if (licenseData[data.license]) {
+    licenseData[data.license] += 1;
+  } else {
+    licenseData[data.license] = 1;
+  }
+
+  appendFileSync(licensesMdPath, `${data.name} | ${data.version} | ${data.license} | ${authorString}
+`)
+})
+
+appendFileSync(licensesMdPath, `\n`)
+
+Object.keys(licenseData).forEach(license => {
+  appendFileSync(licensesMdPath, `* ${license}: ${licenseData[license]}\n`)
+})
